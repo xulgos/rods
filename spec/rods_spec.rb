044cb1719a458e20b76c368d97bce96d87ed5143
Juan Sanchez <jsanchez@dreyfous.com>
2015-02-24 16:59:55 -0400

    rods is now a module to create a name space and change so passing a file  to open is optional

---
+++ spec/rods_spec.rb
@@ -0,0 +1,7 @@
+describe Rods::Document do
+
+  it 'should not throw an error when constructed with not arguments' do
+    Rods::Document.new
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+$:.unshift File.dirname __FILE__
+require 'bundler/setup'
+require 'rods'
+require 'minitest/autorun'
